7ab7f13722f80fb27e80fd87d16a82edb4721f36
james linden <7298563+lamesjinden@users.noreply.github.com>
2024-09-02 01:28:37 -0400

    Support 'fancy?' key to allow a VM to opt out of fancy startup/shutdown

--- src/scheduling/shutdown.clj
+++ src/scheduling/shutdown.clj
@@ -2,11 +2,19 @@
   (:require [clojure.edn :as edn]
             [scheduling.vbox :as vbox]))
 
+(defn standard-shutdown [{vm-name :name}]
+  (vbox/stop-machine-force! vm-name))
+
 (defn fancy-shutdown [{vm-name :name}]
   (vbox/delete-recent-off-snapshot! vm-name)
   (vbox/delete-recent-running-snapshot! vm-name)
   (vbox/take-snapshot! vm-name vbox/snapshot-on-suffix)
   (vbox/stop-machine-wait! vm-name 120))
+
+(defn run-shutdown [{fancy? :fancy? :as vm}]
+  (if (false? fancy?)
+    (standard-shutdown vm)
+    (fancy-shutdown vm)))
 
 (defn -main [& args]
   (let [config-file (or (first args) "config.edn")
@@ -16,4 +24,4 @@
     (->> (get-in config [:virtual-machines])
          (map #(assoc % :state (vbox/machine-state (:name %))))
          (filter #(vbox/running? (:state %)))
-         (run! fancy-shutdown))))
+         (run! run-shutdown))))
--- src/scheduling/startup.clj
+++ src/scheduling/startup.clj
@@ -2,11 +2,19 @@
   (:require [clojure.edn :as edn]
             [scheduling.vbox :as vbox]))
 
+(defn standard-startup [{vm-name :name start-type :start-type}]
+  (vbox/start-machine! vm-name start-type))
+
 (defn fancy-startup [{vm-name :name start-type :start-type}]
   (vbox/delete-recent-shutdown-snapshot! vm-name)
   (vbox/take-snapshot! vm-name vbox/snapshot-off-suffix)
   (vbox/restore-running-snapshot! vm-name)
   (vbox/start-machine! vm-name start-type))
+
+(defn run-startup [{fancy? :fancy? :as vm}]
+  (if (false? fancy?)
+    (standard-startup vm)
+    (fancy-startup vm)))
 
 (defn -main [& args]
   (let [config-file (or (first args) "config.edn")
@@ -16,4 +24,4 @@
     (->> (get-in config [:virtual-machines])
          (map #(assoc % :state (vbox/machine-state (:name %))))
          (filter #(vbox/shutdown? (:state %)))
-         (run! fancy-startup))))
+         (run! run-startup))))
